c5a4c55e602074c9c5e80adc6a22216e67d30477
Saad Ahmed <saad.24.ahm@gmail.com>
2023-06-03 14:04:18 +0100

    added react to front end and configured ports for server and client

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 // App
 const express = require('express'); // gives routes functions
 const app = express(); // connect routes to app
-const port = process.env.PORT || 3000; // for run on port console.log()
+const port = process.env.PORT || 3001; // for run on port console.log()
 
 // Movie
-const Movie = require('../models/movieModel'); // Each movie follows the movie model
-const movieRouter = require('../routes/movieRouter')(Movie); // Add Movie to inject Movie model into the Movie Router / and seperate different Models
+const Movie = require('./models/movieModel'); // Each movie follows the movie model
+const movieRouter = require('./routes/movieRouter')(Movie); // Add Movie to inject Movie model into the Movie Router / and seperate different Models
 app.use(movieRouter)
 
 // Mongoose

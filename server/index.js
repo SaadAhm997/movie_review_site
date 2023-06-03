00ac30406d019ac8afbeb3fe6df8917a6e642591
Saad Ahmed <saad.24.ahm@gmail.com>
2023-06-03 13:41:04 +0100

    checkpoint: added server and changed port

--- server/index.js
+++ server/index.js
@@ -4,17 +4,13 @@ const app = express(); // connect routes to app
 const port = process.env.PORT || 3000; // for run on port console.log()
 
 // Movie
-const Movie = require('./models/movieModel'); // Each movie follows the movie model
-const movieRouter = require('./routes/movieRouter')(Movie); // Add Movie to inject Movie model into the Movie Router / and seperate different Models
+const Movie = require('../models/movieModel'); // Each movie follows the movie model
+const movieRouter = require('../routes/movieRouter')(Movie); // Add Movie to inject Movie model into the Movie Router / and seperate different Models
 app.use(movieRouter)
 
 // Mongoose
 const mongoose = require('mongoose'); // gives mongoose database
 const db = mongoose.connect('mongodb://localhost/movie_review_site'); // mongosh movie_review_site < moviesJson.js  (is the command to link database)
-
-
-
-
 
 app.listen(port, () => {
     console.log(`running on port ${port}`);
